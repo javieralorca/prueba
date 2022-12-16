3386a5e483397fc14f972d38c6e98a636db6c061
Javiera Lorca <jplorca@uc.cl>
2022-12-16 20:07:00 -0300

    integrar js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,7 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
+$('#enviarCorreo').click(function(){
+    alert("El mensaje fue enviado correctamente")
+    })
