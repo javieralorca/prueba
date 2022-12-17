d9957a29aebd41124be9adebb79338f13ba28900
Javiera Lorca <jplorca@uc.cl>
2022-12-17 19:35:38 -0300

    barra fija

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,8 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
 
+
+
 $('#enviarCorreo').click(function(){
     alert("El mensaje fue enviado correctamente")
     })
