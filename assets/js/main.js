7f4669e980608063d6b2e010174acbf1df0d20a0
Javiera Lorca <jplorca@uc.cl>
2022-12-18 13:28:00 -0300

    popover

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
 
 
 
 $('#enviarCorreo').click(function(){
     alert("El mensaje fue enviado correctamente")
     })
+
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
+
+      $('.popover-dismiss').popover({
+        trigger: 'focus'
+      })
